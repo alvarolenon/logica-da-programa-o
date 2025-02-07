62c575bcb853edf1ce63170f2b1c239757c13396
Alvaro lenon <713alvarolenon@gmail.com>
2025-02-07 15:19:47 -0300

    adição de consoles e mudança do numero secreto

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 alert('boas vindas ao jogo');
-let numerosecreto = 29;
+let numerosecreto = 30;
 console.log(numerosecreto)
 let chute = prompt('Escolha um numero entre 1 e 30');
+console.log(`resultado da comparação:`, chute == numerosecreto);
+// se o chute for igual ao numero secreto 
+
 if (numerosecreto == chute) {alert(`parabens, acertou na mosca ${numerosecreto}`);}
 else {alert('voce errouuu :(')}
