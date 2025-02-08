039726a40a305274ea24fd0ccfea6a281af7c8a2
Alvaro lenon <713alvarolenon@gmail.com>
2025-02-07 22:43:57 -0300

    adição de while e contador de tentativas

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 alert('boas vindas ao jogo');
-let numerosecreto = 30;
+let numerosecreto = 25;
 console.log(numerosecreto)
-let chute = prompt('Escolha um numero entre 1 e 30');
-console.log(`resultado da comparação:`, chute == numerosecreto);
-// se o chute for igual ao numero secreto 
+let chute;
+let tentativas = 1;
 
-if (numerosecreto == chute) {alert(`parabens, acertou na mosca ${numerosecreto}`);}
-else {alert('voce errouuu :(')}
+while (chute != numerosecreto) {
+    chute = prompt('Escolha um numero entre 1 e 30');
+    if (numerosecreto == chute) {alert(`parabens, acertou na mosca ${numerosecreto} com ${tentativas} tentativas`);}
+   else {
+        if (chute < numerosecreto) {
+            alert(`o numero secreto é maior que  ${chute}`);
+   } else {
+        alert(`o numero secreto é menor que ${chute}`)
+   }
+   //tentativas = tentativas + 1;
+   tentativas++;
+   }
+   
+}
